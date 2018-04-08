326b9999cd0b28cb3ab605cba1f5e1d37e07315b
Muthu Kumar <muthukumar@thefeathers.in>
2018-04-08 10:02:56 +0530

    [select] Select Class, and change to API

--- Select.js
+++ Select.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const Select = (value, resolve) => ({
-	value,
-	for: (test, fn) => {
-		if (resolve) return {
-			...Select(value, resolve),
-			resolve
-		};
-		if (test) return Select(value, fn);
-		return Select(value);
-	},
-	resolve: resolve ? resolve : () => undefined
-});
+const isIterable = val => typeof val === "object" && Symbol.iterator in val;
+
+class Select {
+	constructor(value, resolve) {
+		this.value = value;
+		this.iterable = isIterable(value);
+		if (resolve) this.resolve = resolve;
+	}
+
+	for(test, consequent) {
+		if (test(this.value)) return new Select(this.value, consequent);
+		if (this.resolve) return this;
+		return this;
+	}
+}
+
+Select.prototype.resolve = () => null;
 
 module.exports = Select;
